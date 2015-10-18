c58678fedfc28fa79b961b78356cdc74ce453211
yanganto <yanganto@gmail.com>
2015-10-18 20:28:13 +0800

    Add symbole table and selection Mode symbole table is the same order with the desktop Array input method

--- js/main.js
+++ js/main.js
@@ -2,12 +2,17 @@ var inputContext = null;
 var keyboardElement;
 var indexString = "";
 var enMod = 0;
-var selectKeys;
 var vibrate = true;
+var selectKeys;
+var partKeys;
+var selectionMod = false;
+var selections = "";
+var selectionIndex = 0;
 
 function init() {
   keyboardElement = document.getElementById('keyboard');
   selectKeys = document.querySelectorAll('.key-select');
+  partKeys = document.querySelectorAll('.key-part');
 
   window.navigator.mozInputMethod.oninputcontextchange = function() {
     inputContext = navigator.mozInputMethod.inputcontext;
@@ -30,8 +35,6 @@ function init() {
   }
 
   keyboardElement.addEventListener('mousedown', function onMouseDown(evt) {
-  // Prevent loosing focus to the currently focused app
-  // Otherwise, right after mousedown event, the app will receive a focus event.
     evt.preventDefault();
   });
 
@@ -66,11 +69,15 @@ function init() {
   var sendKeyElement = document.getElementById('sendKey');
   sendKeyElement.addEventListener('click', function sendKeyHandler() {
     if (indexString){
-      var word = searchWords( true );
-      sendKey( word.charCodeAt(0) );
-      resetSelectKey();
-      indexString = "";
-      renewKeyGroup();  
+			if(selectionMod){
+				nextSelection();	
+			}else{
+				var word = searchWords( true );
+				sendKey( word.charCodeAt(0) );
+				resetSelectKey();
+				indexString = "";
+				renewKeyGroup();  
+			}
     }else{
       sendKey(32);  
     }
@@ -87,26 +94,16 @@ function init() {
       indexString = "";
       renewKeyGroup();
       resetSelectKey();
+			if(selectionMod) {
+				exitSelectionMode();
+			}
     }else{
       sendKey(8);
     }
   });
 
   function listenerForSelectKey( i ) {
-    selectKeys[i].addEventListener('click', function selectKeyHandler( e ) {
-      if( e.target.innerHTML){
-        if ( [ '，', '（', '＃', '＋', '♀', '＄', '▁', '①', 'Α', 'ㄅ'].indexOf(e.target.innerHTML) > -1){
-          // go into symbol mode
-          sendKey( 'Ω'.charCodeAt(0) );
-          
-        }else{ 
-          sendKey( e.target.innerHTML.charCodeAt(0) );
-          resetSelectKey();
-          indexString = "";
-        }
-        renewKeyGroup();
-      }
-    });
+    selectKeys[i].addEventListener('click', selectKeyHandler);
   }
   for ( i = 0; i< selectKeys.length; i++) {
     listenerForSelectKey(i);
@@ -122,31 +119,11 @@ function init() {
   });
   
   deleteKey.addEventListener('touchend', function longHandler() {
-    deletePress = false
+    deletePress = false;
   });
 
-  var partKeys = document.querySelectorAll('.key-part');
-  function arrayInput(c ) { 
-    if ( c == 'i' && indexString.length < 5 || indexString.length < 4 ) {
-      indexString += c;
-      var words = searchWords( false );
-      for ( i = 0; i< selectKeys.length; i++) {
-        selectKeys[i].innerHTML = words[i];
-      } 
-    }else{
-      indexString = "";
-      resetSelectKey();
-    }
-  }
   function listenerForPartKeys( i ) {
-    partKeys[i].addEventListener('click', function partKeyHangler(e) {
-        if ( enMod ) {
-          sendKey(e.target.innerHTML.charCodeAt(0));
-        }else{
-          arrayInput( e.target.innerHTML );
-          renewKeyGroup();
-        }
-    });
+    partKeys[i].addEventListener('click', partKeyHandler );
   }
   for (i = 0; i < partKeys.length; i++) {
       listenerForPartKeys(i);
@@ -170,6 +147,41 @@ function init() {
   switchElement.addEventListener('touchend', function longHandler() {
     clearTimeout(menuTimeout);
   });
+}
+function selectKeyHandler( e ) {
+  if( e.target.innerHTML){
+    if ( [ '，', '（', '＃', '＋', '♀', '＄', '▁', '①', 'Α', 'ㄅ'].indexOf(e.target.innerHTML) > -1){
+			selections = symbolSelections(e.target.innerHTML);
+			enterSelectionMode();
+		}else{ 
+			sendKey( e.target.innerHTML.charCodeAt(0) );
+			indexString = "";
+			renewKeyGroup();
+			if (selectionMod){
+				exitSelectionMode();		
+			}else{
+			  resetSelectKey();
+			}
+		}
+	}
+}
+
+function partKeyHandler(e) {
+	if ( enMod ) {
+		sendKey(e.target.innerHTML.charCodeAt(0));
+	}else{
+			if ( e.target.innerHTML == 'i' && indexString.length < 5 || indexString.length < 4 ) {
+			indexString += e.target.innerHTML;
+			var words = searchWords( false );
+			for ( var i = 0; i< selectKeys.length; i++) {
+				selectKeys[i].innerHTML = words[i] || '';
+			} 
+		}else{
+			indexString = "";
+			resetSelectKey();
+		}
+		renewKeyGroup();
+	}
 }
 
 function resizeWindow() {
@@ -276,7 +288,7 @@ function resetSelectKey() {
 
 function searchWords( space ){
   if (space) return '榮';
-  var mock =[]
+  var mock =[];
   switch( indexString.length){
     case 1:
       mock = quickSearch(indexString);
@@ -291,102 +303,135 @@ function searchWords( space ){
       mock = ['','','','','','','榮','','',''];
       break;
   }
-  return mock
+  return mock;
+}
+
+function enterSelectionMode(){
+  selectionMod = true;
+	selectKeys = document.querySelectorAll('.key-select, .key-part'); 
+  selectionIndex = 0;
+	for( var i = 0; i < partKeys.length; i++){
+		partKeys[i].removeEventListener('click', partKeyHandler);
+		partKeys[i].addEventListener('click', selectKeyHandler);
+	}
+  for( var i = 0; i < selectKeys.length; i++){ 
+    selectKeys[i].innerHTML = selections[i] || "";
+  }
+  selectionIndex += 40;
+	document.getElementById('sendKey').innerHTML = "▽";
+}
+
+function nextSelection(){
+	if ( selectionIndex > selections.length) {
+		selectionIndex = 0;
+	}
+  for( var i = 0; i < selectKeys.length; i++){ 
+    selectKeys[i].innerHTML = selections[i + selectionIndex ] || "";
+  }
+  selectionIndex += 40;
+}
+function exitSelectionMode(){
+  selectionMod = false;
+  selectKeys = document.querySelectorAll('.key-select');
+	for( var i = 0; i < partKeys.length; i++){
+		partKeys[i].removeEventListener('click', selectKeyHandler);
+		partKeys[i].addEventListener('click', partKeyHandler);
+	}
+	for ( var i = 0; i < partKeys.length; i++){
+		partKeys[i].innerHTML = "qwertyuiopasdfghjkl;zxcvbnm,./"[i]
+	}
+	resetSelectKey();
+	document.getElementById('sendKey').innerHTML =  '';
 }
 
 function quickSearch( ch ) {
   switch (ch) {
     case 'a':
-      return ['一', '到', '聽', '現', '政', '弄', '兩', '而', '面', '要']
-      break;
+      return ['一', '到', '聽', '現', '政', '弄', '兩', '而', '面', '要'];
     case 'b':
-      return ['又', '力', '屬', '居', '發', '屋', '通', '習', '務', '局']
-      break;
+      return ['又', '力', '屬', '居', '發', '屋', '通', '習', '務', '局'];
     case 'c':
-      return ['小', '卜', '水', '法', '決', '注', '當', '對', '省', '常']
-      break;
+      return ['小', '卜', '水', '法', '決', '注', '當', '對', '省', '常'];
     case 'd':
-      return ['山', '片', '！', '「', '」', '、', '“', '”', '（', '）']
-      break;
+      return ['山', '片', '！', '「', '」', '、', '“', '”', '（', '）'];
     case 'e':
-      return ['門', '止', '鬥', '開', '關', '鬧', '些', '閱', '處', '桌']
-      break;
+      return ['門', '止', '鬥', '開', '關', '鬧', '些', '閱', '處', '桌'];
     case 'f':
-      return ['十', '莊', '落', '著', '華', '萬', '真', '花', '敬', '故']
-      break;
+      return ['十', '莊', '落', '著', '華', '萬', '真', '花', '敬', '故'];
     case 'g':
-      return ['石', '戶', '也', '那', '破', '孩', '遍', '驗', '承', '啟']
-      break;
+      return ['石', '戶', '也', '那', '破', '孩', '遍', '驗', '承', '啟'];
     case 'h':
-      return ['方', '病', '施', '痛', '良', '遊', '族', '於', '為', '旗']
-      break;
+      return ['方', '病', '施', '痛', '良', '遊', '族', '於', '為', '旗'];
     case 'i':
-      return ['金', '半', '並', '鎮', '食', '拿', '前', '美', '道', '會']
-      break;
+      return ['金', '半', '並', '鎮', '食', '拿', '前', '美', '道', '會'];
     case 'j':
-      return ['目', '刀', '角', '周', '眼', '運', '解', '肉', '色', '免']
-      break;
+      return ['目', '刀', '角', '周', '眼', '運', '解', '肉', '色', '免'];
     case 'k':
-      return ['人', '入', '八', '做', '他', '進', '你', '坐', '作', '個']
-      break;
+      return ['人', '入', '八', '做', '他', '進', '你', '坐', '作', '個'];
     case 'l':
-      return ['竹', '看', '師', '和', '第', '種', '向', '答', '我', '的']
-      break;
+      return ['竹', '看', '師', '和', '第', '種', '向', '答', '我', '的'];
     case 'm':
-      return ['貝', '夕', '貼', '財', '夠', '賠', '體', '贈', '然', '過']
-      break;
+      return ['貝', '夕', '貼', '財', '夠', '賠', '體', '贈', '然', '過'];
     case 'n':
-      return ['之', '心', '定', '麼', '字', '忙', '家', '應', '寫', '空']
-      break;
+      return ['之', '心', '定', '麼', '字', '忙', '家', '應', '寫', '空'];
     case 'o':
-      return ['手', '斤', '臼', '無', '把', '接', '興', '推', '學', '動']
-      break;
+      return ['手', '斤', '臼', '無', '把', '接', '興', '推', '學', '動'];
     case 'p':
-      return ['日', '曰', '田', '時', '最', '是', '照', '點', '易', '國']
-      break;
+      return ['日', '曰', '田', '時', '最', '是', '照', '點', '易', '國'];
     case 'q':
-      return ['工', '七', '車', '哥', '事', '較', '敢', '頭', '或', '區']
-      break;
+      return ['工', '七', '車', '哥', '事', '較', '敢', '頭', '或', '區'];
     case 'r':
-      return ['土', '士', '廿', '起', '地', '老', '帶', '報', '都', '臺']
-      break;
+      return ['土', '士', '廿', '起', '地', '老', '帶', '報', '都', '臺'];
     case 's':
-      return ['乙', '鄉', '收', '跳', '跟', '響', '逃', '飛', '路', '踢']
-      break;
+      return ['乙', '鄉', '收', '跳', '跟', '響', '逃', '飛', '路', '踢'];
     case 't':
-      return ['隨', '民', '巴', '書', '張', '院', '強', '除', '群', '陽']
-      break;
+      return ['隨', '民', '巴', '書', '張', '院', '強', '除', '群', '陽'];
     case 'u':
-      return ['月', '皿', '縣', '腦', '助', '臉', '服', '勝', '胞', '腳']
-      break;
+      return ['月', '皿', '縣', '腦', '助', '臉', '服', '勝', '胞', '腳'];
     case 'v':
-      return ['木', '機', '極', '村', '根', '校', '想', '來', '格', '查']
-      break;
+      return ['木', '機', '極', '村', '根', '校', '想', '來', '格', '查'];
     case 'w':
-      return [ '，', '（', '＃', '＋', '♀', '＄', '▁', '①', 'Α', 'ㄅ' ]
-      break;
+      return [ '，', '（', '＃', '＋', '♀', '＄', '▁', '①', 'Α', 'ㄅ' ];
     case 'x':
-      return ['風', '幾', '經', '結', '級', '將', '能', '給', '總', '約']
-      break;
+      return ['風', '幾', '經', '結', '級', '將', '能', '給', '總', '約'];
     case 'y':
-      return ['立', '言', '裡', '新', '記', '該', '認', '說', '話', '就']
-      break;
+      return ['立', '言', '裡', '新', '記', '該', '認', '說', '話', '就'];
     case 'z':
-      return ['不', '大', '夫', '雨', '成', '在', '布', '願', '原', '電']
-      break;
+      return ['不', '大', '夫', '雨', '成', '在', '布', '願', '原', '電'];
     case '.':
-      return ['。', '身', '行', '街', '很', '往', '愛', '從', '後', '得']
-      break;
+      return ['。', '身', '行', '街', '很', '往', '愛', '從', '後', '得'];
     case '/':
-      return ['四', '虫', '？', '『', '』', '．', '–', '＊', '／', '…']
-      break;
+      return ['四', '虫', '？', '『', '』', '．', '–', '＊', '／', '…'];
     case ';':
-      return ['口', '：', '；', '叫', '呢', '嗎', '吹', '別', '吃', '號']
-      break;
+      return ['口', '：', '；', '叫', '呢', '嗎', '吹', '別', '吃', '號'];
     case ',':
-      return ['，', '火', '米', '精', '燈', '料', '鄰', '勞', '類', '營']
-      break;
+      return ['，', '火', '米', '精', '燈', '料', '鄰', '勞', '類', '營'];
   }
 }
+
+function symbolSelections ( ch ){
+	switch(ch){
+    case '，':
+      return "，、。．‧；：？！︰…‥﹐﹑﹒·﹔﹕﹖﹗｜–︱—︳╴︴﹏";
+    case '（':
+      return "（）︵︶｛｝︷︸〔〕︹︺【】︻︼《》︽︾〈〉︿﹀「」﹁﹂『』﹃﹄﹙﹚﹛﹜﹝﹞‘’“”〝〞‵′";
+    case '＃': 
+      return "＃＆＊※§〃○●△▲◎☆★◇◆□■▽▼㊣℅¯￣＿ˍ﹉﹊﹍﹎﹋﹌﹟﹠﹡";
+    case '＋':
+      return "＋－×÷±√＜＞＝≦≧≠∞≒≡﹢﹣﹤﹥﹦～∩∪⊥∠∟⊿㏒㏑∫∮∵∴";
+    case '♀':
+      return "♀♂⊕⊙↑↓←→↖↗↙↘∥∣／＼∕﹨";
+    case '＄':
+      return "＄￥〒￠￡％＠℃℉﹩﹪﹫㏕㎜㎝㎞㏎㎡㎎㎏㏄°兙兛兞兝兡兣嗧瓩糎";
+    case '▁':
+      return "▁▂▃▄▅▆▇█▏▎▍▌▋▊▉┼┴┬┤├▔─│▕┌┐└┘╭╮╰╯═╞╪╡◢◣◥◤╱╲╳╔╦╗╠╬╣╚╩╝╒╤╕╞╪╡╘╧╛╓╥╖╟╫╢╙╨╜║═╭╮╰╯￭";
+    case '①':
+      return "①②③④⑤⑥⑦⑧⑨⑩⑴⑵⑶⑷⑸⑹⑺⑻⑼⑽ⅰⅱⅲⅳⅴⅵⅶⅷⅸⅹⅠⅡⅢⅣⅤⅥⅦⅧⅨⅩ〡〢〣〤〥〦〧〨〩十卄卅";
+    case 'Α':
+      return "ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩαβγδεζηθικλμνξοπρστυφχψω";
+    case 'ㄅ':
+      return "ㄅㄆㄇㄈㄉㄊㄋㄌㄍㄎㄏㄐㄑㄒㄓㄔㄕㄖㄗㄘㄙㄚㄛㄜㄝㄞㄟㄠㄡㄢㄣㄤㄥㄦㄧㄨㄩ˙ˉˊˇˋ";
+	}
+} 
 
 window.addEventListener('load', init);
